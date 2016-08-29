35f06907fbee90411c7c75575c5ecfa87ab5d5f3
Yoshi Tashiro <tashiro@roomsfor.hk>
2016-08-29 11:37:53 -0400

    [ADD] sale_view_adjust_cft

---
+++ sale_view_adjust_cft/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Ecosoft Co. Ltd.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Sale View Adjust CFT",
+    "version": "8.0.0.5.0",
+    "author": "Ecosoft Co. Ltd.",
+    "license": "AGPL-3",
+    "description": """
+    """,
+    "category": "Sales",
+    "depends": [
+        'crm',
+    ],
+    "data": [
+        'security/crm_security.xml'
+    ],
+    "js": [
+    ],
+    "css": [
+    ],
+    "auto_install": False,
+    "installable": True,
+    "external_dependencies": {
+        'python': [],
+    },
+}
