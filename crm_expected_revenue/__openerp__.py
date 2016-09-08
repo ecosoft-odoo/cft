b058d597d3c6dcdd5404071c055e4808f17ed6e0
nonofpy <pfung@roomsfor.hk>
2016-09-08 05:17:19 -0400

    [ADD]

---
+++ crm_expected_revenue/__openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Multiply  expected revenue by probability",
+    'version': '1.0.0',
+    'author': 'IT-Projects LLC, Ivan Yelizariev',
+    'license': 'GPL-3',
+    'category': 'Custom',
+    'website': 'https://yelizariev.github.io',
+    'depends': ['crm'],
+    'data': [
+        'views.xml',
+    ],
+    'installable': True
+}
