523275ad7e4afa6c9ff05b54876e324e56fa452d
Yoshi Tashiro <tashiro@roomsfor.hk>
2016-09-09 10:04:25 -0400

    [IMP] crm_lead_calculation -> crm_lead_probable_revenue

---
+++ crm_lead_probable_revenue/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Ecosoft Co. Ltd.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "CRM Lead Probable Amount",
+    "summary": "",
+    "version": "8.0.0.5.0",
+    "category": "Sales",
+    "website": "",
+    "author": "Rooms For (Hong Kong) Limited",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "crm",
+    ],
+    "data": [
+        "views/crm_lead_view.xml",
+    ],
+    "demo": [
+    ],
+    "test": [
+    ]
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
