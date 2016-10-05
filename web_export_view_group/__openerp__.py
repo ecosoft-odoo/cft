e663da5331c2c3acfb664f1777bfee15a4f7a266
nonofpy <pfung@roomsfor.hk>
2016-10-05 04:35:03 -0400

    [IMP]web_export_view_group

--- web_export_view_group/__openerp__.py
+++ web_export_view_group/__openerp__.py
@@ -3,20 +3,23 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Limit Excel Button CFT",
+    "name": "Web Export View Group",
     "version": "8.0.1.0.0",
     "author": "Ecosoft Co. Ltd.",
     "license": "AGPL-3",
     "description": """
-        This module is for limiting the access for "Excel Export Button". To assign the user to a new group named
-        Excel Export,the user has accessed to this button to export needed information.
+        This module is for limiting the access
+        for "Excel Export Button". To assign
+        the user to a new group named Excel
+        Export,the user has accessed to this
+        button to export needed information.
     """,
     "category": "Base",
     "depends": [
         'web_export_view',
     ],
     "data": [
-        'security/security_export.xml'
+        'security/web_export_view_security.xml'
     ],
     "js": [
     ],
