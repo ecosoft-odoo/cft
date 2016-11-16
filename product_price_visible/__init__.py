0dc12a77119ad57982c66d084c91df38290ecb96
nonofpy <pfung@roomsfor.hk>
2016-11-16 04:16:16 -0500

    [ADD]adding remarks in the module

--- product_price_visible/__init__.py
+++ product_price_visible/__init__.py
@@ -1,26 +1,6 @@
-# coding: utf-8
-###########################################################################
-#    Module Writen to OpenERP, Open Source Management Solution
-#
-#    Copyright (c) 2012 Vauxoo - http://www.vauxoo.com
-#    All Rights Reserved.
-#    info@vauxoo.com
-############################################################################
-#    Coded by: moylop260 (moylop260@vauxoo.com)
-#    Coded by: isaac (isaac@vauxoo.com)
-############################################################################
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# Copyright 2016 Ecosoft Co. Ltd.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- product_price_visible/__openerp__.py
+++ product_price_visible/__openerp__.py
@@ -1,44 +1,25 @@
-# coding: utf-8
-###########################################################################
-#    Module Writen to OpenERP, Open Source Management Solution
-#
-#    Copyright (c) 2012 Vauxoo - http://www.vauxoo.com
-#    All Rights Reserved.
-#    info@vauxoo.com
-############################################################################
-#    Coded by: moylop260 (moylop260@vauxoo.com)
-#    Coded by: isaac (isaac@vauxoo.com)
-############################################################################
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-#~ "security/ir.model.access.csv",
+# -*- coding: utf-8 -*-
+# Copyright 2016 Ecosoft Co. Ltd.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 {
     "name": "Product Price Visible",
     "version": "8.0.0.1.6",
     "author": "Vauxoo",
     "category": "Product",
-    "website": "www.vauxoo.com",
+    "website": "",
+    "author": "Ecosoft Co. Ltd.",
     "license": "AGPL-3",
+    "application": False,
+    "installable": True,
     "depends": [
         "stock_account",
     ],
-    "demo": [],
     "data": [
         "security/product_price_visible.xml",
         "product_view.xml"
+    ],
+    "demo": [
     ],
     "test": [],
     "js": [],
@@ -47,3 +28,4 @@
     "installable": True,
     "auto_install": False,
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
