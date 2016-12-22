1ae3b7e7a865c66e730c4803e7bae2a989228e87
Yoshi Tashiro <tashiro@roomsfor.hk>
2016-12-22 13:42:50 +0800

    [FIX] version number

--- model_security_adjust_cft/__openerp__.py
+++ model_security_adjust_cft/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Model Security Adjust CFT",
-    "version": "8.0.2.3.0",
+    "version": "8.0.2.2.0",
     "author": "Ecosoft Co. Ltd.",
     "license": "AGPL-3",
     "description": """
