200ddc0c5d3ad73383d200a2ac6da4d617aecdcd
nonofpy <pfung@roomsfor.hk>
2017-01-06 10:15:28 +0800

    [IMP]add the comment

--- account_billing_adjust_cft/__init__.py
+++ account_billing_adjust_cft/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 Ecosoft Co. Ltd.
+# Copyright 2017 Ecosoft Co. Ltd.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import models
--- account_billing_adjust_cft/models/account_billing.py
+++ account_billing_adjust_cft/models/account_billing.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 Ecosoft Co. Ltd.
+# Copyright 2017 Ecosoft Co. Ltd.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api, _
+from openerp import models, fields, api
 
 
 class AccountBillingAdjust(models.Model):
     _inherit = 'account.billing'
 
+# overriding the method to remove billing_date_condition
+# so that all the outstanding invoices for the customer
+# can be listed up when applying billing function
     @api.multi
     def onchange_partner_id(self, company_id,
                             partner_id, currency_id, date):
