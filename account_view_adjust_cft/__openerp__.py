757621bb310d5472c8aae38566f139cf21661a6d
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-01-19 18:36:30 +0800

    [FIX] regression of manifest file

--- account_view_adjust_cft/__openerp__.py
+++ account_view_adjust_cft/__openerp__.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 Ecosoft Co. Ltd.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
 {
     "name": "Account View Adjust CFT",
-    "version": "8.0.1.2.0",
+    "version": "8.0.1.5.0",
     "author": "Ecosoft Co. Ltd.",
     "license": "AGPL-3",
     "description": """
@@ -12,10 +11,14 @@
     "category": "Account",
     "depends": [
         'account_voucher',
-        'account'
+        'product_price_visible',
+        'account_billing',
     ],
     "data": [
         'views/account_voucher_pay_invoice.xml',
+        'views/account_menuitem.xml',
+        'views/account_billing.xml',
+        'views/voucher_payment_receipt_view.xml',
         'views/report_view.xml',
     ],
     "js": [
