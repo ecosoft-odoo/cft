2390e2b05035ccd547a79bdb4104430f8b99b12d
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-01-29 16:52:37 +0800

    [ADD] crm_claim_adjust_cft

---
+++ crm_claim_adjust_cft/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Rooms For (Hong Kong) Limited T/A OSCG
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Adjustments to CRM Claim",
+    "version": "8.0.1.0.0",
+    "author": "Rooms For (Hong Kong) Limited",
+    "website": "https://www.odoo-asia.com",
+    "license": "AGPL-3",
+    "description": """
+    """,
+    "category": "CRM",
+    "depends": [
+        'crm_claim',
+    ],
+    "data": [
+        'views/crm_claim_views.xml'
+    ],
+    "js": [
+    ],
+    "css": [
+    ],
+    "auto_install": False,
+    "installable": True,
+    "external_dependencies": {
+        'python': [],
+    },
+}
