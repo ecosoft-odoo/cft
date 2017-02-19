7a16887d19b8758da9fcfaa7d0ed1f3be57cebdd
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-02-19 18:42:00 +0800

    [FIX] partner name & address presentation

--- account_view_adjust_cft/__openerp__.py
+++ account_view_adjust_cft/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account View Adjust CFT",
-    "version": "8.0.1.5.0",
+    "version": "8.0.1.5.1",
     "author": "Ecosoft Co. Ltd.",
     "license": "AGPL-3",
     "description": """
@@ -19,7 +19,7 @@
         'views/account_menuitem.xml',
         'views/account_billing.xml',
         'views/voucher_payment_receipt_view.xml',
-        'views/report_view.xml',
+        'views/report_invoice.xml',
     ],
     "js": [
     ],
