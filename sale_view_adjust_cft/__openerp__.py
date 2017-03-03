5ab00ecd2f8ae8112c5372837a078ac805a58414
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-03-03 22:50:18 +0800

    [IMP] add group_partner_verification

--- sale_view_adjust_cft/__openerp__.py
+++ sale_view_adjust_cft/__openerp__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 Ecosoft Co. Ltd.
+# Copyright 2016-2017 Ecosoft Co. Ltd.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Sale View Adjust CFT",
-    "version": "8.0.1.2.0",
+    "version": "8.0.1.3.0",
     "author": "Ecosoft Co. Ltd.",
     "license": "AGPL-3",
     "description": """
@@ -17,6 +17,7 @@
     ],
     "data": [
         'security/crm_security.xml',
+        'security/partner_security.xml',
         'data/ir.model.access.csv',
         'views/crm_lead_view.xml',
         'views/crm_phonecall_view.xml',
