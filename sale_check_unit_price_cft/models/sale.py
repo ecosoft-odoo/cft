135ac23393e722febf101b8e09c6e3ca0a4d566a
Kitti U <kittiu@gmail.com>
2017-03-28 15:28:30 +0700

    [ENH] sale_check_unit_price_cft, move warnring as constrains

--- sale_check_unit_price_cft/models/sale.py
+++ sale_check_unit_price_cft/models/sale.py
@@ -9,21 +9,19 @@ from openerp.exceptions import ValidationError
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    @api.multi
-    def action_button_confirm(self):
-        for sale in self:
-            for line in sale.order_line:
-                if line.product_uom_qty:
-                    raw_price = line.price_subtotal / line.product_uom_qty
-                    round_price = float_round(line.price_subtotal /
-                                              line.product_uom_qty, 2)
-                    if float_compare(raw_price, round_price, 4) != 0:
-                        raise ValidationError(
-                            _("Your discount %s%% result in unit price after "
-                              "discount = %s (> 2 decimal points).\n"
-                              "You should adjust unit price manually!") %
-                            (line.discount, raw_price, ))
-        return super(SaleOrder, self).action_button_confirm()
-
+    @api.one
+    @api.constrains('order_line')
+    def _check_unit_price_discount(self):
+        for line in self.order_line:
+            if line.product_uom_qty:
+                raw_price = line.price_subtotal / line.product_uom_qty
+                round_price = float_round(line.price_subtotal /
+                                          line.product_uom_qty, 2)
+                if float_compare(raw_price, round_price, 4) != 0:
+                    raise ValidationError(
+                        _("%s\nYour discount %s%% result in unit price after "
+                          "discount = %s (> 2 decimal points).\n"
+                          "You should adjust unit price manually!") %
+                        (line.name, line.discount, raw_price, ))
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
