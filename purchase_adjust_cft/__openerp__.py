16e41f29798b1db6f137028b9b8bc4b96b4fdf16
newtratip <tharathip.chaweewongphan@gmail.com>
2017-09-01 11:16:36 +0700

    Feature #485 Add menu Currencies to Purchase Module

---
+++ purchase_adjust_cft/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Purchase Adjust CFT",
+    "version": "8.0.1.0.0",
+    "author": "Tharathip C.",
+    "license": "AGPL-3",
+    "description": """
+    """,
+    "category": "Purchase",
+    "depends": [
+        'purchase',
+    ],
+    "data": [
+        'views/res_currency_view.xml',
+    ],
+    "auto_install": False,
+    "installable": True,
+    'application': False,
+}
