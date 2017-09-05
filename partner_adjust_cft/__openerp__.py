729ba89112425e92a512ce05e185ab8152cb7efe
newtratip <tharathip.chaweewongphan@gmail.com>
2017-09-05 10:52:01 +0700

    Feature #533 Contact's Salesperson and Salesteam to inherit data from its parent company

---
+++ partner_adjust_cft/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Partner Adjust CFT",
+    "version": "8.0.1.0.0",
+    "author": "Tharathip C.",
+    "license": "AGPL-3",
+    "description": """
+    """,
+    "category": "Hidden",
+    "depends": [
+        'sales_team',
+    ],
+    "data": [
+        'views/res_partner_view.xml'
+    ],
+    "auto_install": False,
+    "installable": True,
+    "application": False,
+}
