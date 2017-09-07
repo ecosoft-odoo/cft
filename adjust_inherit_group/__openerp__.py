9df8290d7b93c01a69ee4022fcb18a2410fe3286
newtratip <tharathip.chaweewongphan@gmail.com>
2017-09-08 00:12:19 +0700

    Feature #479 New User group for new Employee

---
+++ adjust_inherit_group/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "CFT: Adjust Inherit Group",
+    "version": "8.0.1.0.0",
+    "author": "Tharathip C.",
+    "license": "AGPL-3",
+    "description": """
+    """,
+    "category": "Hidden",
+    "depends": [
+        'group_new_employee',
+    ],
+    "data": [
+        'security/adjust_inherit_group_security.xml',
+    ],
+    "auto_install": False,
+    "installable": True,
+    "application": False,
+}
