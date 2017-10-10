51021a5a13e2be003cced8d4a7b024b25c3816dd
newtratip <tharathip.chaweewongphan@gmail.com>
2017-10-11 00:01:12 +0700

    Feature #466 :Adjust: Add group menu mass mailing for only marketing groups

---
+++ mass_mailing_adjust_cft/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'CFT :: Mass Mailing Adjust',
+    'description': """
+    """,
+    'version': '8.0.1.0.0',
+    'author': 'Tharathip C.',
+    'website': 'http://www.ecosoft.co.th',
+    'category': 'Marketing',
+    'depends': [
+        'mass_mailing',
+        'group_new_employee',
+    ],
+    'data': [
+        'views/mass_mailing_view.xml',
+    ],
+    'qweb': [],
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
