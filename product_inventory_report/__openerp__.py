cce79f9570de2a6267cd869da8d1652b115acec9
newtratip <tharathip.chaweewongphan@gmail.com>
2018-04-26 16:56:12 +0700

    [ADD] Add product inventory report

---
+++ product_inventory_report/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Aged Inventory by Product',
+    'version': '8.0.1.0.0',
+    'category': 'Warehouse Management',
+    'author': 'Tharathip C.',
+    'website': 'http://www.ecosoft.co.th',
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'views/product_inventory_report_view.xml',
+        'wizards/product_inventory_report_wizard.xml',
+    ],
+    'active': False,
+    'application': False,
+    'installable': True,
+}
---
+++ product_inventory_report/wizards/product_inventory_report_wizard.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api
+from openerp.tools.translate import _
+
+
+class ProductInventoryReportWizard(models.TransientModel):
+    _name = 'product.inventory.report.wizard'
+
+    age_inventory = fields.Integer(
+        string='Age Inventory',
+    )
+
+    @api.multi
+    def open_inventory(self):
+        self.ensure_one()
+        self._cr.execute("""
+            SELECT id
+            FROM product_inventory_report
+            WHERE DATE_PART('day', '%s' - in_date) + 1 >= %s"""
+                         % (fields.Date.today(), self.age_inventory))
+        report_ids = list(map(lambda l: l[0], self._cr.fetchall()))
+        return {
+            'name': _('Inventory Report'),
+            'view_type': 'form',
+            'view_mode': 'tree',
+            'res_model': 'product.inventory.report',
+            'view_id': self.env.ref(
+                        'product_inventory_report'
+                        '.view_product_inventory_report_tree').id,
+            'type': 'ir.actions.act_window',
+            'domain': [('id', 'in', report_ids)],
+        }
