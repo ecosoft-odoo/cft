9954277a161d1e43011d654e2b5fc744fe357e8c
som <jutamatk@ecosoft.co.th>
2018-06-26 16:10:34 +0700

    update sale force cancel cft

---
+++ sale_force_cancel_cft/sale.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api
+
+
+class sale_order(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def force_cancel(self):
+        self.write({'state': 'cancel'})
+        return True
