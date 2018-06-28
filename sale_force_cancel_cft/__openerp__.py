e26e5f8ad532e78555fb11aa57dc2da71e49efe9
som <jutamatk@ecosoft.co.th>
2018-06-28 10:14:44 +0700

    modified data

--- sale_force_cancel_cft/__openerp__.py
+++ sale_force_cancel_cft/__openerp__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "sale_force_cancel_cft",
+    'name': 'sale_force_cancel_cft',
     'author': "Jutamat K",
     'category': 'Sales Management',
     'version': '8.0.1.0.0',
