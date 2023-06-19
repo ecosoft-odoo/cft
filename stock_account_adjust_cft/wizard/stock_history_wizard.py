60b174abd9794fd4d74c54d4210da1f752cb80a4
newtratip <tharathip.chaweewongphan@gmail.com>
2023-06-19 16:03:12 +0700

    Add filter date from and date to in wizard of stock history

--- stock_account_adjust_cft/wizard/stock_history_wizard.py
+++ stock_account_adjust_cft/wizard/stock_history_wizard.py
@@ -5,13 +5,19 @@ from openerp import models, fields, api
 class StockHistoryWizard(models.TransientModel):
     _name = 'stock.history.wizard'
 
+    date_from = fields.Date(
+        string='Date From',
+    )
+    date_to = fields.Date(
+        string='Date To',
+    )
     product_ids = fields.Many2many(
         'product.product',
         string='Products',
     )
     product_categ_ids = fields.Many2many(
         'product.category',
-        string='Product Categorys',
+        string='Categories',
     )
     partner_ids = fields.Many2many(
         'res.partner',
@@ -25,14 +31,17 @@ class StockHistoryWizard(models.TransientModel):
         action = self.env.ref('stock_account.action_history_tree')
         result = action.read()[0]
         domain = []
+        if self.date_from:
+            domain += [('date', '>=', self.date_from)]
+        if self.date_to:
+            domain += [('date', '<=', self.date_to)]
         if self.product_ids:
             domain += [('product_id', 'in', self.product_ids.ids)]
         if self.product_categ_ids:
             domain += [('product_categ_id', 'in', self.product_categ_ids.ids)]
         if self.partner_ids:
             domain += [('partner_id', 'in', self.partner_ids.ids)]
-        if domain:
-            result.update({
-                'domain': domain,
-            })
+        result.update({
+            'domain': domain,
+        })
         return result
