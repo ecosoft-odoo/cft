355da30995b0fc242be25eef70e6222acf414156
newtratip <tharathip.chaweewongphan@gmail.com>
2023-10-04 11:26:22 +0700

    [ENH] l10n_th_amount_text_ext: Add unreconcile total text th and en on billing page

--- l10n_th_amount_text_ext/all_docs.py
+++ l10n_th_amount_text_ext/all_docs.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from openerp.osv import osv
+from openerp.osv import osv, fields
 from openerp.addons.l10n_th_amount_text.all_docs import AmountToWord
 
 
@@ -22,6 +22,10 @@ class AmountToWordExt(AmountToWord):
                                  dr_line.amount_retention +
                                  dr_line.amount_wht)
             amount_total = abs(amount_total)
+        elif obj._name == 'account.billing':
+            # Billing will sum amount from unreconciled amount
+            for cr_line in obj.line_cr_ids:
+                amount_total += cr_line.amount_unreconciled
         return amount_total
 
 
@@ -43,5 +47,36 @@ class account_invoice(AmountToWordExt, osv.osv):
 class account_voucher(AmountToWordExt, osv.osv):
 
     _inherit = 'account.voucher'
+
+
+class account_billing(AmountToWordExt, osv.osv):
+    _inherit = 'account.billing'
+
+    def _amount_unreconciled_total_text_en(
+            self, cursor, user, ids, name, arg, context=None):
+        return self._amount_to_word_en(cursor, user, ids, name,
+                                       arg, context=context)
+
+    def _amount_unreconciled_total_text_th(
+            self, cursor, user, ids, name, arg, context=None):
+        return self._amount_to_word_th(cursor, user, ids, name,
+                                       arg, context=context)
+
+    _columns = {
+        'amount_unreconciled_total_text_en': fields.function(
+            _amount_unreconciled_total_text_en,
+            string='Amount Unreconciled Total (EN)', type='char',
+            store={'account.billing':
+                   (lambda self, cr, uid, ids, c={}:
+                    ids, ['line_cr_ids'], 100),
+                   }),
+        'amount_unreconciled_total_text_th': fields.function(
+            _amount_unreconciled_total_text_th,
+            string='Amount Unreconciled Total (TH)', type='char',
+            store={'account.billing':
+                   (lambda self, cr, uid, ids, c={}:
+                    ids, ['line_cr_ids'], 100),
+                   }),
+    }
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
